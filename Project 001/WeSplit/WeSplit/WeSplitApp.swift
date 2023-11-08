b5999cbe3d9d2d65ce39712629bc0cbaf6b83372
Steven McWee <stevenmcwee@gmail.com>
2023-11-08 13:49:05 +0000

    WeSplit: Initial commit

---
+++ Project 001/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Steven McWee on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

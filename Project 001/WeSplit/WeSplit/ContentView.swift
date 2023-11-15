2465d1ea92bf53587f5856b0f0846c33384440ab
Steven McWee <stevenmcwee@gmail.com>
2023-11-15 11:14:06 +0000

    Views and Modifiers: Challenge 1 - Make WeSplit total red if tipping 0%

--- Project 001/WeSplit/WeSplit/ContentView.swift
+++ Project 001/WeSplit/WeSplit/ContentView.swift
@@ -54,6 +54,7 @@ struct ContentView: View {
                 
                 Section("Check total") {
                     Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .foregroundStyle(tipPercentage == 0 ? .red : .black)
                 }
                 
                 Section("Amount per person") {

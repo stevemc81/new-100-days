cff31c819500a9e410e7d1e972db61a1aabace25
Steven McWee <stevenmcwee@gmail.com>
2023-11-20 10:41:34 +0000

    Rock Paper Scissors: UI built

--- Milestone 001/RockPaperScissors/RockPaperScissors/ContentView.swift
+++ Milestone 001/RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -8,14 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["🪨", "📃", "✂️"]
+    
+    @State private var move = Int.random(in: 0...2)
+    @State private var outcome = Bool.random()
+    @State private var score = 0
+    @State private var questionsAsked = 0
+    @State private var endOfGame = false
+    @State private var showingScore = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(colors: [.blue, .mint], center: .center, startRadius: 0, endRadius: 400)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Rock, Paper, Scissors!")
+                    .font(.largeTitle.bold())
+                
+                Spacer()
+                
+                Text("Computer picks...")
+                    .font(.title)
+                Text("\(moves[move])")
+                    .font(.system(size: 200))
+                Text("And wants you to...")
+                    .font(.title)
+                Text(outcome ? "WIN!" : "LOSE!")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.bold())
+                
+                Spacer()
+                
+                Text("Make your choice!")
+                    .font(.title)
+                HStack {
+                    ForEach(0..<3) { selection in
+                        Button {
+                            // Do something
+                        } label: {
+                            Text(moves[selection])
+                        }
+                        .font(.system(size: 75))
+                    }
+                }
+                
+                Spacer()
+                
+                Text("Score ???")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/.bold())
+            }
         }
-        .padding()
     }
 }
 

a4e3b5b07e149e76584309fd374b64fef3209240
Steven McWee <stevenmcwee@gmail.com>
2023-12-04 16:02:46 +0000

    WordScramble: Initial commit

---
+++ Project 005/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Steven McWee on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

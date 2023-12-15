d2fe7f01fbb2711102d86549649a2fe7a39ae4f3
Steven McWee <stevenmcwee@gmail.com>
2023-12-15 15:36:21 +0000

    Animations - initial commit

---
+++ Project 006/Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Steven McWee on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
